51c8c88a6ef6d5e183512975756dd2b17cb80760
ivanzar <zarubinevan@yandex.ru>
2018-09-30 23:04:19 +0300

    Update TODO1

--- plugins/committeePlugin/script/committee.js
+++ plugins/committeePlugin/script/committee.js
@@ -33,3 +33,38 @@ function getCommitteeRequestsList()
 
     }
 }
+
+function createCommitteRequest()
+{
+
+    var wif = $('#committee_create_wif').val();
+    var creator = $('#committee_create_creator').val();
+    var url = $('#committee_create_url').val();
+    var worker = $('#committee_create_worker').val();
+    var reward_amount_min =
+            Number.parseFloat($('#committee_create_reward_min').val()) + ' VIZ';
+    var reward_amount_max =
+            Number.parseFloat($('#committee_create_reward_max').val()) + ' VIZ';
+    var duration =
+            Number.parseFloat($('#committee_create_duration').val()) * 60 * 60 * 24;
+
+    console.log(wif, creator, url, worker,
+            reward_amount_min, reward_amount_max, duration);
+
+    broadcast.committeeWorkerCreateRequest(wif, creator, url, worker,
+            reward_amount_min, reward_amount_max, duration, callback);
+
+    function callback(err, result)
+    {
+        
+        console.log(err, result, result.length);
+        if (!err)
+        {
+            $('#committee_create-out').html('Создан');
+        } else
+        {
+            alert(err);
+        }
+
+    }
+}
