9f7207bab8eeef68420f5152907f38c7e4b6cf07
ivanzar <zarubinevan@yandex.ru>
2018-10-09 23:34:34 +0300

    add auto chnge node

---
+++ scripts/viz_config.js
@@ -0,0 +1,8 @@
+var socket = new WebSocket("wss://testnet.viz.world");
+socket.onerror = function (err){
+	viz.config.set('websocket', 'wss://ws.viz.ropox.tools');
+	console.log(err);
+};
+
+
+viz.config.set('websocket', 'wss://ws.viz.ropox.tools');
